a6a89a174c454835475fda6839a3adb537727ddf
Dell <Dell@DESKTOP-URB9PIG>
2020-10-25 16:48:55 -0600

    Nuevo Patroness

---
+++ src/data/Patron.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author working
+ */
+public class Patron {
+
+    private String clase;
+    private String claseResultante;
+    private double[] vectorC;
+    private int num_clas;
+
+    public Patron() {
+        
+    }    
+    
+    public Patron(String clase, String claseResultante, double[] vectorC) {
+        this.clase = clase;
+        this.claseResultante = claseResultante;
+        this.vectorC = vectorC;
+        this.num_clas = num_clas;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    /**
+     * @param clase the clase to set
+     */
+    public void setClase(String clase) {
+        this.clase = clase;
+    }
+
+    /**
+     * @return the claseResultante
+     */
+    public String getClaseResultante() {
+        return claseResultante;
+    }
+
+    /**
+     * @param claseResultante the claseResultante to set
+     */
+    public void setClaseResultante(String claseResultante) {
+        this.claseResultante = claseResultante;
+    }
+
+    /**
+     * @return the vectorC
+     */
+    public double[] getVectorC() {
+        return vectorC;
+    }
+
+
+    public void setVectorC(double[] vectorC) {
+        this.vectorC = vectorC;
+    }
+
+    public int getNum_clas() {
+        return num_clas;
+    }
+
+    public void setNum_clas(int num_clas) {
+        this.num_clas = num_clas;
+    }
+
+}
